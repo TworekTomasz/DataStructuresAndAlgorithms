86b53dd269f73aaa99532c34fe869c8434821991
Tomasz Tworek <tomasztworek27@gmail.com>
2024-01-11 18:27:25 +0100

    Added linked list example

---
+++ src/main/java/CustomLinkedList.java
@@ -0,0 +1,159 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class CustomLinkedList implements List {
+
+    private Node head;
+
+    private static class Node {
+        Object value;
+
+        Node next;
+
+        public Node() {
+        }
+
+        public Node(Object value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public CustomLinkedList() {
+        this.head = null;
+    }
+
+    public CustomLinkedList(Node head) {
+        this.head = head;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        Node newNode = new Node(o, null);
+
+        if (head == null) {
+            head = newNode;
+            return true;
+        }
+
+        Node current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+
+        current.next = newNode;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList customLinkedList = new CustomLinkedList();
+        customLinkedList.add(2);
+        customLinkedList.add(4);
+        customLinkedList.add(8);
+        customLinkedList.add(1);
+
+    }
+}
